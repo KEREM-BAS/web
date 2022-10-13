096cd3c9cf569f78996872f4612094f701145ed2
KEREM BAS <kerembas2000@gmail.com>
2022-10-13 20:04:45 +0300

    main

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,19 +3,18 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "5e009eac37c5f334efe9ddde398e9a3e",
-"assets/assets/images/1920x600.png": "069d3025cf987aec9f5fccd400675272",
+  "assets/AssetManifest.json": "df90b7097e2a71f72b2c904d212a53f5",
 "assets/assets/images/1920x850.png": "6912a25d50786c886760d605e42699f3",
 "assets/assets/images/bus.png": "ba92222d512fd6c9ac577e38cbfe5239",
 "assets/assets/images/call-center-service.png": "a46112326ac45339d43117ed6cf43730",
 "assets/assets/images/dhi-steps.png": "f52bb6d946a74c72cf5f576e97ae5397",
 "assets/assets/images/fuedhilogo.png": "9affc9cbc964413fbf8ac5bbc4400fbe",
+"assets/assets/images/fuedhi_logo.png": "161f89a02a5bcd03d73865e5fb3baecc",
 "assets/assets/images/hair-transplant.png": "8532ac2aaba541270ddeb6283e08b12e",
 "assets/assets/images/hotel.png": "19baed8b3c6815c9f87cbdbe50145eef",
 "assets/assets/images/PRP.png": "7b0643f513399797296c47cecbc716d5",
 "assets/assets/images/toiletries.png": "3fe79ef1fc974f4ede4bb05ef60b469c",
 "assets/assets/images/translator.png": "ed1164b9d9e45aa574ea3e58031ee65d",
-"assets/assets/images/Untitled.png": "399d553dcdd03e6321679763d5cf4c6d",
 "assets/assets/images/welcome-to-dental-bosphorus.jpg": "ffe859eaacc78fb5eeef6305128e120e",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
@@ -324,9 +323,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "599cc9b6b658a0bb7eea896a5e2edc81",
-"/": "599cc9b6b658a0bb7eea896a5e2edc81",
-"main.dart.js": "a0310771f55ddc28ac28a00ba139d3b7",
+"index.html": "9a413133cf3e2af161caebea393b7dfb",
+"/": "9a413133cf3e2af161caebea393b7dfb",
+"main.dart.js": "2e24254871fd06a6c5d37ed1b89847b5",
 "manifest.json": "4c4e1c2232bb11604884a88a545e40c6",
 "version.json": "ff8625bd1bd1163ace3beed1a0a5c6c5"
 };
