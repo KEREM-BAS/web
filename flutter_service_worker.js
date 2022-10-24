49d61759e49572bac8aa8ef185cf87d5e885f24f
KEREM BAS <kerembas2000@gmail.com>
2022-10-24 19:19:15 +0300

    main

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -328,9 +328,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a8d4cbe6c194be4cb3d3bb8cb6022d87",
-"/": "a8d4cbe6c194be4cb3d3bb8cb6022d87",
-"main.dart.js": "cf8ae4de73a58261ac982b156d1dfe41",
+"index.html": "cb4780a56ff4a595d86049d68e53368a",
+"/": "cb4780a56ff4a595d86049d68e53368a",
+"main.dart.js": "e59f72c95f3500bb79389b14385f9932",
 "manifest.json": "4c4e1c2232bb11604884a88a545e40c6",
 "version.json": "ff8625bd1bd1163ace3beed1a0a5c6c5"
 };
